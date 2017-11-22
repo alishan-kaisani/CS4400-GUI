8712df88ce8395bfd71b994fc94f23c7e96704f1
Joel Pozin <joelpozin97@gmail.com>
2017-11-21 23:55:50 -0500

    Update backend.py
    Added ViewStations() function and PrettifyViewStations() function, the latter of which makes the result of the ViewStations() function handleable.

--- backend.py
+++ backend.py
@@ -16,7 +16,7 @@ def VerifyLogin(self):
     try:
     	with connection.cursor() as cursor:
     		# We have to somehow hash the password to make sure it's the same in the database
-    		sql = 'SELECT * FROM User WHERE Username = "{}" and Password = "{}"'.format(username, hashlib.md5(password.encode('utf-8')).hexdigest())
+    		sql = 'SELECT * FROM User WHERE Username = "{}" and Password = "{}";'.format(username, hashlib.md5(password.encode('utf-8')).hexdigest())
     		cursor.execute(sql)
             global is_admin
             m = cursor.fetchall()
@@ -34,7 +34,7 @@ def GenerateNewCardNumber():
     Returns a 16-digit integer that is not already in the database."""
     while True:
         newnum = randrange(int(1e15), int(1e16))
-        sql = 'SELECT * FROM Breezecard WHERE BreezecardNum = "{}"'.format(newnum)
+        sql = 'SELECT * FROM Breezecard WHERE BreezecardNum = "{}";'.format(newnum)
         connection = pymysql.connect(host='academic-mysql.cc.gatech.edu',
                                 user = 'cs4400_Group_110',
                                 password = 'KAfx5IQr',
@@ -48,13 +48,13 @@ def GenerateNewCardNumber():
 
 def CreateNewUser(username, email, password, cardnumber=None):
     """Adds a new user to the database; inserts tuples into User, Passenger, and Breezecard tables.
-    Returns 1 if the operation is successful."""
+    Returns 1 if the operation is successful, otherwise an exception is raised."""
     if cardnumber is None:
         cardnumber = GenerateNewCardNumber()
     assert len(str(cardnumber)) == 16, "Card number must be 16 digits"
-    sql = 'INSERT INTO User VALUES ("{}", "{}", false)'.format(username, hashlib.md5(password.encode('utf-8')).hexdigest()) #New users are always passengers, not admins
-    sql2 = 'INSERT INTO Passenger VALUES ("{}", "{}")'.format(username, email)
-    sql3 = 'INSERT INTO Breezecard VALUES ("{}", 0.00, "{}")'.format(cardnumber, username)
+    sql = 'INSERT INTO User VALUES ("{}", "{}", false);'.format(username, hashlib.md5(password.encode('utf-8')).hexdigest()) #New users are always passengers, not admins
+    sql2 = 'INSERT INTO Passenger VALUES ("{}", "{}");'.format(username, email)
+    sql3 = 'INSERT INTO Breezecard VALUES ("{}", 0.00, "{}");'.format(cardnumber, username)
     connection = pymysql.connect(host='academic-mysql.cc.gatech.edu',
                                 user = 'cs4400_Group_110',
                                 password = 'KAfx5IQr',
@@ -74,3 +74,43 @@ def CreateNewUser(username, email, password, cardnumber=None):
     finally:
         connection.close()
         # print("Finished creating new user") #For testing purposes only
+
+def CreateNewUserWrapper(existing_card, username, email, password, *args):
+    """Calls the CreateNewUser function depending on whether the new user is using an existing Breeze Card or wishes to generae a new one.
+    existing_card is a boolean parameter that determines whether the new user already has a breezecard.
+    username, email, and password have the same meaning as they do in CreateNewUser.
+    *args allows for the optional parameter (Breeze Card number) to be called when existing_card == True.
+    Returns 1 if successful, otherwise an exception is raised."""
+    if existing_card == False:
+        CreateNewUser(username, email, password)
+    else:
+        CreateNewUser(username, email, password, args[0])
+    # The CreateNewUser function will return 1, so we do not need to include a return statement in this function
+
+def ViewStations():
+    """Returns a tuple of tuples, where each nested tuple is of the form (Station name, StopID, Decimal('fare amount'), ClosedStatus).
+    If tuple not returned, then exception would have been raised.
+    Use the PrettifyViewStations() function to get rid of annoying """
+    connection = pymysql.connect(host='academic-mysql.cc.gatech.edu',
+                                user = 'cs4400_Group_110',
+                                password = 'KAfx5IQr',
+                                db = 'cs4400_Group_110')
+    sql = 'SELECT Name, StopID, EnterFare, ClosedStatus FROM Station ORDER BY Name ASC;'
+    try:
+        with connection.cursor() as cursor:
+            cursor.execute(sql)
+            m = cursor.fetchall()
+            return m
+    except:
+        print("Something went wrong. Blame Joel.")
+    finally:
+        connection.close()
+
+def PrettifyViewStations():
+    """Makes the station listing from the ViewStations() function usable, since each station's fare will no longer be a Decimal object.
+    Returns a list of several tuples where the Decimal object is replaced by a floating point number (otherwise an exception would have been raised)."""
+    listing = ViewStations()
+    newlisting = []
+    for tup in listing:
+        newlisting.append((tup[0], tup[1], round(float(tup[2]), 2), tup[3]))
+    return newlisting
